d1fe86ba082f3575c72e16a75dc6ea011d4e07d5
(Amanda Orbe) <garayomiriam@gmail.com>
2017-11-29 11:36:48 +0100

    adapted to diferent media + pictures

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,14 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+(function() {
+"use strict";
+
+var variable1 = 2, variable2 = "Hello"
+
+console.log(variable1);
+console.log(variable2);
+
+
+})();
